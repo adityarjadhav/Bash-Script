c0a994d26e7837418cc950fd3d2eb3411b3563b2
adityarjadhav <adityarjadhav@outlook.com>
2023-12-23 22:05:44 +0530

    updated script

--- shell_script.sh
+++ shell_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Define an array of repository directories to search for "azure-pipelines.yml" files
+# Define an array of repository directories to searching for "azure-pipelines.yml" files
 repositories=("tfmod_gcp_dns" "tfmod_gcp_ilb_17_serverless" "dxp_liferay" "tfmod_gcp_gke_nodepool" "tfmod_gop_bq_dataset" "tfmod_gcp_billing_budget" "rubrik_gcp" "portfolio_se_gcp" "tfmod_gcp_net_interconnect_direct_attach" "tfmod_k8_automation" "timod_gcp_gcs" "sandbox_iac_archetype")  # Add your repository directories here
 
 # Content to add to "azure-pipelines.yml" files
@@ -8,10 +8,10 @@ content="paths:
   exclude:
     - README.md"
 
-# Loop through each repository directory
+# Loop through each repo directory
 for repository in "${repositories[@]}"; do
   if [ -d "$repository" ]; then
-    # Use a for loop to find and modify each "azure-pipelines.yml" file
+# Use a loop(for loop) to find and modify each "azure-pipelines.yml" file
     for file in $(find "$repository" -type f -name "azure-pipelines.yml"); do
       # Use sed to append the content after the "trigger" section
       sed -i '/trigger:/a\'"$content" "$file"
